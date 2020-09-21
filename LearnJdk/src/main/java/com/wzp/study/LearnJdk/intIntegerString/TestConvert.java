b553123b02e347d2cb7efc434f95f1f8f9efdbba
万泽鹏 <zepeng.wan@melot.cn>
2020-09-21 11:12:03 +0800

    int Integer String 之间的转换

---
+++ LearnJdk/src/main/java/com/wzp/study/LearnJdk/intIntegerString/TestConvert.java
@@ -0,0 +1,30 @@
+package com.wzp.study.LearnJdk.intIntegerString;
+
+/**
+ * @ClassName : TestConvert
+ * @Description : int Integer String之间的转换
+ * @Author : wanzepeng
+ * @Date: 2020-09-21 11:09
+ */
+public class TestConvert {
+
+    public static void main(String[] args) {
+        int i = '0';
+        System.out.println(i);
+
+        //int integer
+        //integer int
+        //string Integer
+
+        //先用parseInt转int 再用valueOf转Integer
+        Integer integer = Integer.valueOf("2");
+        //装箱再拆箱
+        int i1 = Integer.valueOf("2");
+        int i2 = Integer.parseInt("2");
+
+        //int 转String
+        String s = String.valueOf(i1);
+    }
+
+
+}
