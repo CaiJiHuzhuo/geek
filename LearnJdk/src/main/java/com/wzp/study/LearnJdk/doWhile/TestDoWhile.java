687c84035851b4d75d1a1161900ac8c04cf41934
万泽鹏 <zepeng.wan@melot.cn>
2020-09-22 16:44:45 +0800

    do while奇葩代码测试

---
+++ LearnJdk/src/main/java/com/wzp/study/LearnJdk/doWhile/TestDoWhile.java
@@ -0,0 +1,18 @@
+package com.wzp.study.LearnJdk.doWhile;
+
+/**
+ * @ClassName : TestDoWhile
+ * @Description : do while
+ * @Author : wanzepeng
+ * @Date: 2020-09-22 16:41
+ */
+public class TestDoWhile {
+
+    //奇葩写法测试
+    public static void main(String[] args) {
+        do{
+            System.out.println(1);
+        }while (false);
+//        System.out.println(2);
+    }
+}
