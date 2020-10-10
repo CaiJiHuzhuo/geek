277c9146cf477b856a9362575344f5f075795655
wzp <897551108@qq.com>
2020-10-10 21:13:16 +0800

    SentinelWeb

---
+++ LearnSentinel/src/main/java/com/wzp/study/sentinel/sentinelWeb/SentinelWeb.java
@@ -0,0 +1,13 @@
+package com.wzp.study.sentinel.sentinelWeb;
+
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SentinelWeb {}
