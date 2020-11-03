25d238ec92f2a71014a0afc52fc3a5c311b856b0
万泽鹏 <897551108@qq.com>
2020-11-03 11:02:40 +0800

    rsa加解密算法

---
+++ LearnJdk/src/main/java/com/wzp/study/LearnJdk/encryption/EncryptionException.java
@@ -0,0 +1,49 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2012 All Rights Reserved.
+ */
+package com.wzp.study.LearnJdk.encryption;
+
+
+/**
+ * 
+ * @author runzhi
+ */
+public class EncryptionException extends Exception {
+
+    private static final long serialVersionUID = -238091758285157331L;
+
+    private String            errCode;
+    private String            errMsg;
+
+    public EncryptionException() {
+        super();
+    }
+
+    public EncryptionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EncryptionException(String message) {
+        super(message);
+    }
+
+    public EncryptionException(Throwable cause) {
+        super(cause);
+    }
+
+    public EncryptionException(String errCode, String errMsg) {
+        super(errCode + ":" + errMsg);
+        this.errCode = errCode;
+        this.errMsg = errMsg;
+    }
+
+    public String getErrCode() {
+        return this.errCode;
+    }
+
+    public String getErrMsg() {
+        return this.errMsg;
+    }
+
+}
