33f67900ff16641b13fb9bddc3994a8a6cd339a4
万泽鹏 <897551108@qq.com>
2020-11-10 17:39:22 +0800

    Leetcode31

---
+++ LearnJdk/src/main/java/com/wzp/study/LearnJdk/leetcode/Leetcode31.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2001-2020 melotgroup Corporation Limited. All rights reserved.
+ * This software is the confidential and proprietary information of melotgroup Company.
+ * ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with http://melotgroup.com/.
+ */
+package com.wzp.study.LearnJdk.leetcode;
+
+/**
+ * TODO
+ *
+ * @author admin
+ * @version V1.0
+ * @since 2020-11-10 16:16
+ */
+public class Leetcode31 {
+
+    public static void main(String[] args) {
+        int[] init = new int[]{2,3,1};
+        nextPermutation(init);
+        for (int i :init
+        ) {
+            System.out.print(i+" ");
+        }
+
+    }
+
+    public static void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
---
+++ LearnJdk/src/main/java/com/wzp/study/LearnJdk/leetcode/LeetcodeTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2001-2020 melotgroup Corporation Limited. All rights reserved.
+ * This software is the confidential and proprietary information of melotgroup Company.
+ * ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with http://melotgroup.com/.
+ */
+package com.wzp.study.LearnJdk.leetcode;
+
+/**
+ * TODO
+ *
+ * @author admin
+ * @version V1.0
+ * @since 2020-11-10 16:31
+ */
+public class LeetcodeTest {
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
