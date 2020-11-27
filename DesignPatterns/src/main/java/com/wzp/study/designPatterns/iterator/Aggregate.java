cb150004ac2f4a098ecaa6f33eb387bf45176025
万泽鹏 <897551108@qq.com>
2020-11-27 17:58:06 +0800

    DesignPatterns init

---
+++ DesignPatterns/src/main/java/com/wzp/study/designPatterns/iterator/Aggregate.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2001-2020 melotgroup Corporation Limited. All rights reserved.
+ * This software is the confidential and proprietary information of melotgroup Company.
+ * ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with http://melotgroup.com/.
+ */
+package com.wzp.study.designPatterns.iterator;
+
+/**
+ * TODO
+ *
+ * @author admin
+ * @version V1.0
+ * @since 2020-11-26 10:00
+ */
+public interface Aggregate {
+    void iterator();
+}
