8d5ddc894642f8d1157aee1ff81c736d64560e7a
万泽鹏 <897551108@qq.com>
2021-01-19 16:38:33 +0800

    spring

---
+++ LearnSpring/src/main/java/com/wzp/study/LearnSpring/beanFactory/BeanFactoryTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2001-2021 melotgroup Corporation Limited. All rights reserved.
+ * This software is the confidential and proprietary information of melotgroup Company.
+ * ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with http://melotgroup.com/.
+ */
+package com.wzp.study.LearnSpring.beanFactory;
+
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * TODO
+ *
+ * @author admin
+ * @version V1.0
+ * @since 2021-01-18 11:01
+ */
+public class BeanFactoryTest {
+
+    @Test
+    public void testSimpleLoad(){
+        BeanFactory bf = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
+        MyTestBean myTestBean = (MyTestBean) bf.getBean("myTestBean");
+        System.out.println(myTestBean.getTestStr());
+
+    }
+}
---
+++ LearnSpring/src/main/java/com/wzp/study/LearnSpring/beanFactory/MyTestBean.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2001-2021 melotgroup Corporation Limited. All rights reserved.
+ * This software is the confidential and proprietary information of melotgroup Company.
+ * ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with http://melotgroup.com/.
+ */
+package com.wzp.study.LearnSpring.beanFactory;
+
+/**
+ * TODO
+ *
+ * @author admin
+ * @version V1.0
+ * @since 2021-01-18 10:59
+ */
+public class MyTestBean {
+
+    private String testStr = "testStr";
+
+    public String getTestStr() {
+        return testStr;
+    }
+
+    public void setTestStr(String testStr) {
+        this.testStr = testStr;
+    }
+}
