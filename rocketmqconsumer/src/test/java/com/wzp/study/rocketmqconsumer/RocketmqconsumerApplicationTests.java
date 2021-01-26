16db0eb26fbc31a9a2005b08ad8884768fa722ca
万泽鹏 <897551108@qq.com>
2021-01-26 17:01:09 +0800

    mq consumer

---
+++ rocketmqconsumer/src/test/java/com/wzp/study/rocketmqconsumer/RocketmqconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzp.study.rocketmqconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
