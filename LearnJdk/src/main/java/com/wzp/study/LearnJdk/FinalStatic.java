192456b4b5734ee29cd5cc72f8577b0e701e01ba
万泽鹏 <897551108@qq.com>
2021-02-26 11:47:54 +0800

    final static

---
+++ LearnJdk/src/main/java/com/wzp/study/LearnJdk/FinalStatic.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2001-2021 melotgroup Corporation Limited. All rights reserved.
+ * This software is the confidential and proprietary information of melotgroup Company.
+ * ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with http://melotgroup.com/.
+ */
+package com.wzp.study.LearnJdk;
+
+/**
+ * TODO
+ *
+ * @author admin
+ * @version V1.0
+ * @since 2021-02-26 09:54
+ */
+public class FinalStatic {
+
+    /**
+     * final 修饰的属性不可修改
+     *       修饰的方法不可重写
+     *       修饰的类不可继承
+     * static 类属性
+     *         类方法
+     */
+}
